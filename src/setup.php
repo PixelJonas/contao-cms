5ebddbb6b97e2c4d6369a97c1e2ec1b6f3a8b8db
pixeljonas <jonas@janz.digital>
2020-05-16 10:55:31 +0200

    📝 remove installation specific configuration

--- src/setup.php
+++ src/setup.php
@@ -2,4 +2,3 @@
 // Provide some variables for properly running behind non http proxy.
 $_SERVER['HTTPS'] = 0;
 $_SERVER['ssl'] = false;
-$_SERVER['SERVER_NAME'] = 'localhost';
--- system/config/localconfig.php
+++ system/config/localconfig.php
@@ -2,8 +2,6 @@
 
 ### INSTALL SCRIPT START ###
 $GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
-$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$12$C1zub0RjLGGAQvaW.vXvMuC3qXfPCHTRDgB1CpYj8PtHoPXgL4wEW';
-$GLOBALS['TL_CONFIG']['adminEmail'] = 'jonas@janz.digital';
 $GLOBALS['TL_CONFIG']['allowedTags'] = '<a><abbr><acronym><address><area><article><aside><audio><b><bdi><bdo><big><blockquote><br><base><button><canvas><caption><cite><code><col><colgroup><data><datalist><dataset><dd><del><dfn><div><dl><dt><em><fieldset><figcaption><figure><footer><form><h1><h2><h3><h4><h5><h6><header><hgroup><hr><i><iframe><img><input><ins><kbd><keygen><label><legend><li><link><map><mark><menu><nav><object><ol><optgroup><option><output><p><param><picture><pre><q><s><script><samp><section><select><small><source><span><strong><style><sub><sup><table><tbody><td><textarea><tfoot><th><thead><time><tr><tt><u><ul><var><video><wbr>';
 $GLOBALS['TL_CONFIG']['subcolumns'] = 'yaml3';
 $GLOBALS['TL_CONFIG']['subcolumns_gapdefault'] = 40;
@@ -12,7 +10,6 @@ $GLOBALS['TL_CONFIG']['imageWidth'] = 8000;
 $GLOBALS['TL_CONFIG']['imageHeight'] = 8000;
 $GLOBALS['TL_CONFIG']['minifyMarkup'] = true;
 $GLOBALS['TL_CONFIG']['gzipScripts'] = true;
-$GLOBALS['TL_CONFIG']['websiteTitle'] = 'Schönberg Herzogtum Lauenburg';
 $GLOBALS['TL_CONFIG']['gdMaxImgWidth'] = 8000;
 $GLOBALS['TL_CONFIG']['gdMaxImgHeight'] = 8000;
 ### INSTALL SCRIPT STOP ###
